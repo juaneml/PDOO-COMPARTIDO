57cdbc433818b0b3d9430e43f1d6753ab813345d
Juan Esteban <juaneml@correo.ugr.es>
2016-01-07 10:41:09 +0100

    up

--- Juan/P3/Ruby/Napakalaki/lib/examen.rb
+++ Juan/P3/Ruby/Napakalaki/lib/examen.rb
@@ -1,38 +1,38 @@
 ## To change this license header, choose License Headers in Project Properties.
 ## To change this template file, choose Tools | Templates
 ## and open the template in the editor.
-#require 'singleton'
-#require 'Player.rb'
-#require 'CardDealer.rb'
-#require 'Dice.rb'
-#require 'BadConsequence'
-#
-#require 'singleton'
-#module NapakalakiGame
-#class Examen
-#    include Singleton
-#    
-#    private
-#    def initialize
-#        @player= Player.new("prueba")
-#    end
-#    
-#    public
-#    def run
-#        badconsequence = BadConsequence.newLevelSpecificTreasures('Prueba',3,[TreasureKind::ONEHAND,TreasureKind::ONEHAND,TreasureKind::BOTHHANDS],[TreasureKind::SHOES])
-#        
-#        
-#        v= @player.getVisibleTreasures
-#        h= @player.getHiddenTreasures
-#        
-#        puts v
-#        puts h
-#        badconsequence.adjustToFitTreasureList(v, h)
-#        puts v
-#        puts h
-#        
-#    end
-#    
-#    
-#end
-#end
+require 'singleton'
+require 'Player.rb'
+require 'CardDealer.rb'
+require 'Dice.rb'
+require 'BadConsequence'
+
+require 'singleton'
+module NapakalakiGame
+class Examen
+    include Singleton
+    
+    private
+    def initialize
+        @player= Player.new("prueba")
+    end
+    
+    public
+    def run
+        badconsequence = BadConsequence.newLevelSpecificTreasures('Prueba',3,[TreasureKind::ONEHAND,TreasureKind::ONEHAND,TreasureKind::BOTHHANDS],[TreasureKind::SHOES])
+        
+        
+        v= @player.getVisibleTreasures
+        h= @player.getHiddenTreasures
+        
+        puts v
+        puts h
+        badconsequence.adjustToFitTreasureList(v, h)
+        puts v
+        puts h
+        
+    end
+    
+    
+end
+end

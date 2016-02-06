2293e289c8e4a92d24ede9de50c672309e5055e4
raulrbugr <raulruizbueno@gmail.com>
2016-02-06 09:44:30 +0100

    P5 nuevo main
    Nuevo main de la p5

---
+++ Juan/P5/java/Napakalaki/src/NapakalakiGame/NapakalakiMain.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package NapakalakiGame;
+
+import GUI.*;
+/**
+ *
+ * @author Raul
+ */
+public class NapakalakiMain {
+    
+    public static void main(String[] args) {
+        
+        Napakalaki game = Napakalaki.getInstance();
+        NapakalakiView napakalakiView = new NapakalakiView();
+        
+        Dice.createInstance(napakalakiView);
+        
+        napakalakiView.setNapakalaki(game);
+        napakalakiView.setVisible(true);
+        
+    }
+    
+}

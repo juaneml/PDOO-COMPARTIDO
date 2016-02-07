a3b72a7b9a66531150454de7a6698a449c4ba962
Juan Esteban <juaneml@correo.ugr.es>
2016-02-07 17:09:14 +0100

    P5 H completo

--- Juan/P5/java/Napakalaki/src/GUI/NapakalakiView.java
+++ Juan/P5/java/Napakalaki/src/GUI/NapakalakiView.java
@@ -38,46 +38,98 @@ public class NapakalakiView extends javax.swing.JFrame {
         player_monster = new javax.swing.JPanel();
         currentMonster = new GUI.MonsterView();
         currentPlayer = new GUI.PlayerView();
+        meet_monster = new javax.swing.JToggleButton();
+        combat = new javax.swing.JToggleButton();
+        next_turn = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("NAPAKALAKI");
 
         principal.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-        principal.setLayout(new java.awt.BorderLayout());
 
         javax.swing.GroupLayout player_monsterLayout = new javax.swing.GroupLayout(player_monster);
         player_monster.setLayout(player_monsterLayout);
         player_monsterLayout.setHorizontalGroup(
             player_monsterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(player_monsterLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(currentPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0)
+                .addComponent(currentPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, 1158, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(currentMonster, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 0, 0))
         );
         player_monsterLayout.setVerticalGroup(
             player_monsterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(player_monsterLayout.createSequentialGroup()
-                .addGap(0, 0, 0)
-                .addGroup(player_monsterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(currentMonster, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(currentPlayer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(0, 0, 0))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, player_monsterLayout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addGroup(player_monsterLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(currentPlayer, javax.swing.GroupLayout.PREFERRED_SIZE, 591, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(currentMonster, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22))
         );
 
-        principal.add(player_monster, java.awt.BorderLayout.CENTER);
+        meet_monster.setText("Meet the Monster");
+        meet_monster.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                meet_monsterActionPerformed(evt);
+            }
+        });
+
+        combat.setText("Combat");
+        combat.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                combatActionPerformed(evt);
+            }
+        });
+
+        next_turn.setText("Next Turn");
+
+        javax.swing.GroupLayout principalLayout = new javax.swing.GroupLayout(principal);
+        principal.setLayout(principalLayout);
+        principalLayout.setHorizontalGroup(
+            principalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(player_monster, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(principalLayout.createSequentialGroup()
+                .addGap(103, 103, 103)
+                .addComponent(meet_monster)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(combat)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(next_turn))
+        );
+        principalLayout.setVerticalGroup(
+            principalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(principalLayout.createSequentialGroup()
+                .addComponent(player_monster, javax.swing.GroupLayout.PREFERRED_SIZE, 591, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
+                .addGroup(principalLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(meet_monster)
+                    .addComponent(combat)
+                    .addComponent(next_turn))
+                .addContainerGap())
+        );
 
         getContentPane().add(principal, java.awt.BorderLayout.CENTER);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void meet_monsterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_meet_monsterActionPerformed
+        //codigo
+    }//GEN-LAST:event_meet_monsterActionPerformed
+
+    private void combatActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_combatActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_combatActionPerformed
+
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton combat;
     private GUI.MonsterView currentMonster;
     private GUI.PlayerView currentPlayer;
+    private javax.swing.JToggleButton meet_monster;
+    private javax.swing.JToggleButton next_turn;
     private javax.swing.JPanel player_monster;
     private javax.swing.JPanel principal;
     // End of variables declaration//GEN-END:variables
